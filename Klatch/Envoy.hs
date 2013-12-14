9090840ae343e1d50a47d3a219a686e07071c4f8
Mikael Brockman <mikael.brockman@gmail.com>
2013-12-14 18:46:05 +0100

    Refactoring and fixing

--- Klatch/Envoy.hs
+++ Klatch/Envoy.hs
@@ -24,7 +24,7 @@ import qualified Data.Map as Map
 
 import Data.Time.Clock.POSIX
 
-import Network.Simple.TCP
+import Network.Simple.TCP (Socket, connect)
 
 import Pipes
 import Pipes.Concurrent
@@ -32,30 +32,6 @@ import qualified Pipes.Parse as PP
 import qualified Pipes.ByteString as PBS
 import qualified Pipes.Prelude as P
 import Pipes.Network.TCP (fromSocket, toSocket)
-
-run :: IO ()
-run = do
-  fleet <- newTVarIO Map.empty
-  channel <- newTChanIO :: IO (TChan Event)
-  void . async . runEffect $
-    for (contents channel) (yield . fromUTF8 . encode) >-> P.stdoutLn
-  runEffect $
-    for P.stdinLn (handle fleet channel . decode . toUTF8)
-
-toUTF8 :: String -> BS.ByteString
-toUTF8 = E.encodeUtf8 . T.pack
-
-fromUTF8 :: BS.ByteString -> String
-fromUTF8 = T.unpack . E.decodeUtf8
-
-fromStrictUTF8 :: SB.ByteString -> String
-fromStrictUTF8 = fromUTF8 . BS.fromChunks . (:[])
-
-toStrictUTF8 :: String -> SB.ByteString
-toStrictUTF8 = SB.concat . BS.toChunks . toUTF8
-
-contents :: TChan a -> Producer' a IO ()
-contents c = forever $ liftIO (atomically $ readTChan c) >>= yield
 
 type Timestamp = Int
 
@@ -67,6 +43,18 @@ data Command = Connect String String String
 data Event = Connected String String String Timestamp
            | Received String String Timestamp
              deriving (Eq, Show, Generic)
+
+newtype Envoy = Envoy { sendTo :: String -> IO () }
+
+type Fleet = TVar (Map String Envoy)
+
+run :: IO ()
+run = do
+  fleet <- newTVarIO Map.empty
+  channel <- newTChanIO :: IO (TChan Event)
+  void $ concurrently
+    (runEffect $ contents channel >-> P.map (fromUTF8 . encode) >-> P.stdoutLn)
+    (runEffect $ for P.stdinLn (handle fleet channel . decode . toUTF8))
 
 instance FromJSON Command where
   parseJSON (Object v) =
@@ -82,41 +70,67 @@ instance FromJSON Command where
 
 instance ToJSON Event where
   toJSON (Connected name host port t) =
-    object ["event" .= ("connected" :: String),
-            "time" .= t,
-            "name" .= name,
-            "host" .= host,
-            "port" .= port]
+    object [ "event" .= ("connected" :: String)
+           , "time" .= t
+           , "name" .= name
+           , "host" .= host
+           , "port" .= port ]
   toJSON (Received name line t) =
-    object ["event" .= ("received" :: String),
-            "time" .= t,
-            "name" .= name,
-            "line" .= line]
-
-timestamped :: (Timestamp -> a) -> IO a
-timestamped f = fmap (f . (`div` 1000000000) . fromEnum) getPOSIXTime
+    object [ "event" .= ("received" :: String)
+           , "time" .= t
+           , "name" .= name
+           , "line" .= line ]
 
 handle :: Fleet -> TChan Event -> Maybe Command -> Effect IO ()
 handle fleet channel (Just (Connect name host port)) =
-  void . liftIO . async $ connect host port $ \(socket, _) -> do
-    event <- liftIO $ timestamped $ Connected name host port
-    atomically . writeTChan channel $ event
+    liftIO . void . async $ connect host port $ \(socket, _) ->
+        onConnect fleet channel socket name host port
 
-    async . runEffect $
-      for (PP.concat . PBS.lines $ fromSocket socket 4096) $ \line ->
-        if line /= ""
-        then do
-          event <- liftIO . timestamped . Received name . fromStrictUTF8 $ line
-          liftIO . atomically $ writeTChan channel event
-        else return ()
+onConnect :: Fleet -> TChan Event -> Socket -> String
+          -> String -> String -> IO ()
+onConnect fleet channel socket name host port = do
+  writeTimestamped channel $ Connected name host port
 
-    (output, input) <- spawn Unbounded
-    liftIO . atomically $ addEnvoy fleet name output
+  async . runEffect . for (socketLines socket) $
+    writeTimestamped channel . Received name
 
-    runEffect $ fromInput input >-> P.map toStrictUTF8 >-> toSocket socket
+  (output, input) <- spawn Unbounded
+  atomically . addEnvoy fleet name $ output
+  runEffect . inputToSocket socket $ input
 
-type Fleet = TVar (Map String (Output String))
+inputToSocket :: Socket -> Input String -> Effect IO ()
+inputToSocket socket input =
+    fromInput input >-> P.map toStrictUTF8 >-> toSocket socket
+
+socketLines :: Socket -> Producer String IO ()
+socketLines s = p >-> P.filter (/= "") >-> P.map fromStrictUTF8
+  where p = PP.concat . PBS.lines $ fromSocket s 4096
+
+contents :: TChan a -> Producer a IO ()
+contents c = forever $ liftIO (atomically $ readTChan c) >>= yield
+
+timestamped :: (Functor m, MonadIO m) => (Timestamp -> a) -> m a
+timestamped f =
+    fmap (f . (`div` 1000000000) . fromEnum) (liftIO getPOSIXTime)
+
+writeTimestamped :: (Functor m, MonadIO m) =>
+                    TChan a -> (Timestamp -> a) -> m ()
+writeTimestamped c f =
+    timestamped f >>= liftIO . atomically . writeTChan c
 
 addEnvoy :: Fleet -> String -> Output String -> STM ()
-addEnvoy f name output =
-  modifyTVar f (Map.insert name output)
+addEnvoy m name output = modifyTVar m . Map.insert name . Envoy $ f
+    where f x = do True <- atomically (send output x)
+                   return ()
+
+toUTF8 :: String -> BS.ByteString
+toUTF8 = E.encodeUtf8 . T.pack
+
+fromUTF8 :: BS.ByteString -> String
+fromUTF8 = T.unpack . E.decodeUtf8
+
+fromStrictUTF8 :: SB.ByteString -> String
+fromStrictUTF8 = fromUTF8 . BS.fromChunks . (:[])
+
+toStrictUTF8 :: String -> SB.ByteString
+toStrictUTF8 = SB.concat . BS.toChunks . toUTF8
