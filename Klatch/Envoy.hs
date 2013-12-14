c70ae1a71981aaa06655775537e5fdc3f31f5b48
Mikael Brockman <mikael.brockman@gmail.com>
2013-12-14 19:10:49 +0100

    Move some stuff to Util module

--- Klatch/Envoy.hs
+++ Klatch/Envoy.hs
@@ -2,10 +2,11 @@
 
 module Klatch.Envoy where
 
+import Klatch.Util
+
 import Control.Applicative
 import Control.Monad
 import Control.Monad.STM
-
 import Control.Concurrent.Async
 import Control.Concurrent.STM.TVar
 import Control.Concurrent.STM.TChan
@@ -13,25 +14,14 @@ import Control.Concurrent.STM.TChan
 import Data.Aeson
 import GHC.Generics (Generic)
 
-import qualified Data.ByteString.Lazy as BS
-import qualified Data.ByteString as SB
-
-import qualified Data.Text.Lazy          as T
-import qualified Data.Text.Lazy.Encoding as E
-
 import           Data.Map   (Map)
 import qualified Data.Map as Map
 
-import Data.Time.Clock.POSIX
-
-import Network.Simple.TCP (Socket, connect)
-
 import Pipes
 import Pipes.Concurrent
-import qualified Pipes.Parse as PP
-import qualified Pipes.ByteString as PBS
-import qualified Pipes.Prelude as P
-import Pipes.Network.TCP (fromSocket, toSocket)
+
+import Data.Time.Clock.POSIX
+import Network.Simple.TCP (Socket, connect)
 
 type Timestamp = Int
 
@@ -82,16 +72,6 @@ run = do
     (writeChannelToStdout channel)
     (forEveryStdinLine $ handle fleet channel)
 
-runEffectsConcurrently :: Effect IO a -> Effect IO b -> IO (a, b)
-runEffectsConcurrently a b = concurrently (runEffect a) (runEffect b)
-
-writeChannelToStdout :: ToJSON a => TChan a -> Effect IO ()
-writeChannelToStdout c =
-    contents c >-> P.map (fromUTF8 . encode) >-> P.stdoutLn
-
-forEveryStdinLine :: FromJSON a => (Maybe a -> IO ()) -> Effect IO ()
-forEveryStdinLine f = for P.stdinLn (liftIO . f . decode . toUTF8)
-
 handle :: Fleet -> TChan Event -> Maybe Command -> IO ()
 handle fleet channel (Just (Connect name host port)) =
     void . async $ connect host port $ \(socket, _) ->
@@ -109,17 +89,6 @@ onConnect fleet channel socket name host port = do
     (for (socketLines socket) $ writeTimestamped channel . Received name)
     (inputToSocket socket $ input)
 
-inputToSocket :: Socket -> Input String -> Effect IO ()
-inputToSocket socket input =
-    fromInput input >-> P.map toStrictUTF8 >-> toSocket socket
-
-socketLines :: Socket -> Producer String IO ()
-socketLines s = p >-> P.filter (/= "") >-> P.map fromStrictUTF8
-  where p = PP.concat . PBS.lines $ fromSocket s 4096
-
-contents :: TChan a -> Producer a IO ()
-contents c = forever $ liftIO (atomically $ readTChan c) >>= yield
-
 timestamped :: (Functor m, MonadIO m) => (Timestamp -> a) -> m a
 timestamped f =
     fmap (f . (`div` 1000000000) . fromEnum) (liftIO getPOSIXTime)
@@ -133,15 +102,3 @@ addEnvoy :: Fleet -> String -> Output String -> STM ()
 addEnvoy m name output = modifyTVar m . Map.insert name . Envoy $ f
     where f x = do True <- atomically (send output x)
                    return ()
-
-toUTF8 :: String -> BS.ByteString
-toUTF8 = E.encodeUtf8 . T.pack
-
-fromUTF8 :: BS.ByteString -> String
-fromUTF8 = T.unpack . E.decodeUtf8
-
-fromStrictUTF8 :: SB.ByteString -> String
-fromStrictUTF8 = fromUTF8 . BS.fromChunks . (:[])
-
-toStrictUTF8 :: String -> SB.ByteString
-toStrictUTF8 = SB.concat . BS.toChunks . toUTF8
---
+++ Klatch/Util.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Klatch.Util where
+
+import Control.Monad
+import Control.Monad.STM
+
+import Control.Concurrent.Async
+import Control.Concurrent.STM.TChan
+
+import Data.Aeson
+
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString as SB
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.Encoding as E
+
+import Network.Simple.TCP (Socket)
+
+import Pipes
+import Pipes.Concurrent (Input, fromInput)
+import Pipes.Network.TCP (fromSocket, toSocket)
+
+import qualified Pipes.Prelude as P
+import qualified Pipes.Parse as PP
+import qualified Pipes.ByteString as PBS
+
+runEffectsConcurrently :: Effect IO a -> Effect IO b -> IO (a, b)
+runEffectsConcurrently a b = concurrently (runEffect a) (runEffect b)
+
+writeChannelToStdout :: ToJSON a => TChan a -> Effect IO ()
+writeChannelToStdout c =
+    contents c >-> P.map (fromUTF8 . encode) >-> P.stdoutLn
+
+forEveryStdinLine :: FromJSON a => (Maybe a -> IO ()) -> Effect IO ()
+forEveryStdinLine f = for P.stdinLn (liftIO . f . decode . toUTF8)
+
+contents :: TChan a -> Producer a IO ()
+contents c = forever $ liftIO (atomically $ readTChan c) >>= yield
+
+inputToSocket :: Socket -> Input String -> Effect IO ()
+inputToSocket socket input =
+    fromInput input >-> P.map toStrictUTF8 >-> toSocket socket
+
+socketLines :: Socket -> Producer String IO ()
+socketLines s = p >-> P.filter (/= "") >-> P.map fromStrictUTF8
+  where p = PP.concat . PBS.lines $ fromSocket s 4096
+
+toUTF8 :: String -> BS.ByteString
+toUTF8 = E.encodeUtf8 . T.pack
+
+fromUTF8 :: BS.ByteString -> String
+fromUTF8 = T.unpack . E.decodeUtf8
+
+fromStrictUTF8 :: SB.ByteString -> String
+fromStrictUTF8 = fromUTF8 . BS.fromChunks . (:[])
+
+toStrictUTF8 :: String -> SB.ByteString
+toStrictUTF8 = SB.concat . BS.toChunks . toUTF8
