5fb117b767c11a0bd15dc49b28ebaae7372b9472
Mikael Brockman <mbrock@goula.sh>
2013-12-21 20:18:51 +0100

    Web client: tweak replay animation

--- web-client/klatch.js
+++ web-client/klatch.js
@@ -180,7 +180,10 @@
     render: function () {
       var curve = Math.pow(this.props.progress, 1.7);
       var ratio = curve * 100;
-      var style = { width: (100 - ratio) + "%"};
+      var style = {
+        width: (100 - ratio) + "%",
+        opacity: (1 - curve)
+      };
       return (
         <div className="replaying">
           <span className="replaying" style={style}></span>
