7c390b4f6accc86cffefe204b30e2cce82d7c7f2
Mikael Brockman <phubuh@gmail.com>
2014-01-03 11:43:48 +0100

    Delete irc-server.sh

--- dummies/irc-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true ; do
-  ( while true ; do
-     echo Hello! I am an IRC server, almost.
-     sleep 1
-     echo I keep saying the same thing.
-     sleep 2
-  done ) | nc -l $PORT
-done
